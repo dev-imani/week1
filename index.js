79cd5475b303c4645983621d8529c6cf684e7e1a
Kel Kel <michaelimani95@gmail.com>
2024-12-27 14:10:50 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// day 1 on learning jvascript
+// date objects ➡️➡️ have values that represent dates and time
+const date = new Date(2004, 3, 8, 1, 70);
+const display1= document.getElementById('display')
+const click = addEventListener('click',function (){
+    display1.textContent = date 
+})
+
+
